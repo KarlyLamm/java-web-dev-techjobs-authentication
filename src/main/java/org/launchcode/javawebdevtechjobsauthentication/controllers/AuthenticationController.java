9c3303f1aafd0b4d5cfae022cf595efe78bbc71b
KarlyLamm <61996747+KarlyLamm@users.noreply.github.com>
2020-07-16 09:06:32 -0500

    imports fixed in controller

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
@@ -1,9 +1,22 @@
 // this controller will deal w/ User objects
 package org.launchcode.javawebdevtechjobsauthentication.controllers;
 
+import org.launchcode.javawebdevtechjobsauthentication.models.User;
 import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepository;
+import org.launchcode.javawebdevtechjobsauthentication.models.dto.LoginFormDTO;
+import org.launchcode.javawebdevtechjobsauthentication.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 public class AuthenticationController {
